b1de4d55796e7ef0123a45dd50f689d149ab5916
Armando Roque <roque510@hotmail.com>
2017-05-10 23:34:51 -0600

    gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+var gulp = require('gulp');
+var browserify = require('gulp-browserify');
+var concat = require('gulp-concat');
+var less = require('gulp-less');
+var refresh = require('gulp-livereload');
+var lr = require('tiny-lr');
+var server = lr();
+var minifyCSS = require('gulp-minify-css');
+var embedlr = require('gulp-embedlr');
+var sass = require('gulp-sass');
+
+//style paths
+var sassFiles = 'assets/styles/sass/**/*.scss',  
+    cssDest = 'assets/styles/css/';
+
+gulp.task('styles', function(){  
+    gulp.src(sassFiles)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(cssDest));
+});
+
+gulp.task('scripts', function() {
+    gulp.src(['app/src/**/*.js'])
+        .pipe(browserify())
+        .pipe(concat('dest.js'))
+        .pipe(gulp.dest('dist/build'))
+        .pipe(refresh(server))
+})
+
+gulp.task('styles', function() {
+    gulp.src(['app/css/style.less'])
+        .pipe(less())
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('dist/build'))
+        .pipe(refresh(server))
+})
+
+gulp.task('lr-server', function() {
+    server.listen(35729, function(err) {
+        if(err) return console.log(err);
+    });
+})
+
+gulp.task('html', function() {
+    gulp.src("app/*.html")
+        .pipe(embedlr())
+        .pipe(gulp.dest('dist/'))
+        .pipe(refresh(server));
+})
+
+gulp.task('default', function() {
+    gulp.run('lr-server', 'scripts', 'styles', 'html');
+
+    gulp.watch('app/src/**', function(event) {
+        gulp.run('scripts');
+    })
+
+    gulp.watch('app/css/**', function(event) {
+        gulp.run('styles');
+    })
+
+    gulp.watch('app/**/*.html', function(event) {
+        gulp.run('html');
+    })
+})
